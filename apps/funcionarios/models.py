5a80b963f7d6e6dd9f3e1469833ac7b2aea7d490
Marshal Mori Cavalheiro <marshalmori@gmail.com>
2019-12-19 00:07:27 -0300

    Adição dos relacionamentos entre registro_hora_extra e Departamento/Empresa

--- apps/funcionarios/models.py
+++ apps/funcionarios/models.py
@@ -1,14 +1,16 @@
 from django.db import models
 from django.contrib.auth.models import User
 from apps.departamentos.models import Departamento
+from apps.empresas.models import Empresa
 
 # Create your models here.
 
 class Funcionario(models.Model):
     nome = models.CharField(max_length=100)
-    user = models.ForeignKey(User, on_delete=models.PROTECT)
-    #user = models.OneToOneField(User, on_delete=models.PROTECT)
+    # user = models.ForeignKey(User, on_delete=models.PROTECT)
+    user = models.OneToOneField(User, on_delete=models.PROTECT)
     departamentos = models.ManyToManyField(Departamento)
+    empresa = models.ForeignKey(Empresa, on_delete=models.PROTECT)
 
 
     def __str__(self):
