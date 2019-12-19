15d951c5977affc160bc63a5b70f5d3f80d37041
Marshal Mori Cavalheiro <marshalmori@gmail.com>
2019-12-18 22:36:26 -0300

    Adição do User do Django na classe Funcionario

--- apps/funcionarios/models.py
+++ apps/funcionarios/models.py
@@ -1,9 +1,12 @@
 from django.db import models
+from django.contrib.auth.models import User
 
 # Create your models here.
 
 class Funcionario(models.Model):
     nome = models.CharField(max_length=100)
+    user = models.ForeignKey(User, on_delete=models.PROTECT)
+    #user = models.OneToOneField(User, on_delete=models.PROTECT)
 
 
     def __str__(self):
