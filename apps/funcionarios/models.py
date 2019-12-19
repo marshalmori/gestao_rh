12495870de94feb684ab348c33ab0fb894d98d51
Marshal Mori Cavalheiro <marshalmori@gmail.com>
2019-12-18 22:44:03 -0300

    Adição da relação Departamento na classe Funcionario

--- apps/funcionarios/models.py
+++ apps/funcionarios/models.py
@@ -1,5 +1,6 @@
 from django.db import models
 from django.contrib.auth.models import User
+from apps.departamentos.models import Departamento
 
 # Create your models here.
 
@@ -7,6 +8,7 @@ class Funcionario(models.Model):
     nome = models.CharField(max_length=100)
     user = models.ForeignKey(User, on_delete=models.PROTECT)
     #user = models.OneToOneField(User, on_delete=models.PROTECT)
+    departamentos = models.ManyToManyField(Departamento)
 
 
     def __str__(self):
