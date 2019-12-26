5459fd5ae21323d2f5b5fd63dc7b1c64f61ad554
Marshal Mori Cavalheiro <marshalmori@gmail.com>
2019-12-25 22:25:03 -0300

    Funcionario edita os dados da empresa

--- apps/empresas/views.py
+++ apps/empresas/views.py
@@ -1,3 +1,4 @@
+from django.http import HttpResponse
 from django.shortcuts import render
 from django.views.generic.edit import CreateView 
 from .models import Empresa
@@ -6,6 +7,14 @@ from .models import Empresa
 class EmpresaCreate(CreateView):
     model = Empresa
     fields = ['nome']
+
+    def form_valid(self, form):
+        obj = form.save()
+        funcionario = self.request.user.funcionario
+        funcionario.empresa = obj
+        funcionario.save()
+        return HttpResponse('Ok')
+
 
 
 
